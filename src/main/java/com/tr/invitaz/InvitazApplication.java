69023ac2e11eca52daf7bf0aa032324944b2d7b8
mehmet.demircioglu <demircioglumhmt@gmailcom>
2019-03-16 18:56:57 +0300

    delete import

--- src/main/java/com/tr/invitaz/InvitazApplication.java
+++ src/main/java/com/tr/invitaz/InvitazApplication.java
@@ -1,6 +1,5 @@
 package com.tr.invitaz;
 
-import com.tr.invitaz.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
